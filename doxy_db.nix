5576f9a5e687eeb7c3fc1a4866781a3e00279fb8
Travis A. Everett <travis.a.everett@gmail.com>
2023-04-20 22:49:33 -0500

    re-enable patched doxygen

--- doxy_db.nix
+++ doxy_db.nix
@@ -21,16 +21,16 @@ let
   # building my version of doxygen first
   doxygen_sqlite3 = doxygen.overrideAttrs (attrs: rec {
     name = "doxygen-1.9.6-sqlite3gen";
-    # src = fetchFromGitHub {
-    #   owner  = "abathur";
-    #   repo   = "doxygen";
-    #   # specific fix commit (now merged, unreleased)
-    #   # Release_1_9_0, Release_1_9_1, Release_1_9_2, Release_1_9_3, Release_1_9_4, Release_1_9_5, Release_1_9_6
-    #   # bad: b8a3ff6c33264c43cdf30c04baa9793e7e8d51a2 592aaa4f17d73ec8c475df0f44efaea8cc4d575c
-    #   # good: 6a7201851a1667da40b4e2a1cf7b481c2d386803 5d0281a264e33ec3477bd7f6a9dcef79a6ef8eeb e03e2a29f9279deabe62d795b0db925a982d0eef
-    #   rev    = "test_schema_version_fix_1_9_6";
-    #   hash   = "sha256-C9UiMSZtEY0cbR0A7TYKyZU8gdYYgDuuW6aDL/bvG5g=";
-    # };
+    src = fetchFromGitHub {
+      owner  = "abathur";
+      repo   = "doxygen";
+      # specific fix commit (now merged, unreleased)
+      # Release_1_9_0, Release_1_9_1, Release_1_9_2, Release_1_9_3, Release_1_9_4, Release_1_9_5, Release_1_9_6
+      # bad: b8a3ff6c33264c43cdf30c04baa9793e7e8d51a2 592aaa4f17d73ec8c475df0f44efaea8cc4d575c
+      # good: 6a7201851a1667da40b4e2a1cf7b481c2d386803 5d0281a264e33ec3477bd7f6a9dcef79a6ef8eeb e03e2a29f9279deabe62d795b0db925a982d0eef
+      rev    = "test_schema_version_fix_1_9_6";
+      hash   = "sha256-C9UiMSZtEY0cbR0A7TYKyZU8gdYYgDuuW6aDL/bvG5g=";
+    };
     buildInputs = attrs.buildInputs ++ [ sqlite ];
     cmakeFlags = [
       "-Duse_sqlite3=ON"
