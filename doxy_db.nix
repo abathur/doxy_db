44f2957d17361451416e0208381ac87587a35575
Travis A. Everett <travis.a.everett@gmail.com>
2023-05-15 21:36:36 -0500

    set up canary CI job
    PR adding sqlite to doxygen in nixpkgs has landed, so I'm
    trimming back how much we override here. Also add flake
    checks and a CI job for testing against nixpkgs master and
    Doxygen's master.

--- doxy_db.nix
+++ doxy_db.nix
@@ -1,7 +1,5 @@
 { python311Packages
 , doxygen
-, fetchFromGitHub
-, sqlite
 , runCommand
 , libxslt
 }:
@@ -18,35 +16,11 @@
 # - python311 works
 # at nixpkgs 8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8 mar 11 2023
 let
-  # building my version of doxygen first
-  doxygen_sqlite3 = doxygen.overrideAttrs (attrs: rec {
-    name = "doxygen-1.9.6-sqlite3gen";
-    src = fetchFromGitHub {
-      owner  = "abathur";
-      repo   = "doxygen";
-      # specific fix commit (now merged, unreleased)
-      # Release_1_9_0, Release_1_9_1, Release_1_9_2, Release_1_9_3, Release_1_9_4, Release_1_9_5, Release_1_9_6
-      # bad: b8a3ff6c33264c43cdf30c04baa9793e7e8d51a2 592aaa4f17d73ec8c475df0f44efaea8cc4d575c
-      # good: 6a7201851a1667da40b4e2a1cf7b481c2d386803 5d0281a264e33ec3477bd7f6a9dcef79a6ef8eeb e03e2a29f9279deabe62d795b0db925a982d0eef
-      rev    = "test_schema_version_fix_1_9_6";
-      hash   = "sha256-C9UiMSZtEY0cbR0A7TYKyZU8gdYYgDuuW6aDL/bvG5g=";
-    };
-    buildInputs = attrs.buildInputs ++ [ sqlite ];
-    cmakeFlags = [
-      "-Duse_sqlite3=ON"
-    ] ++ attrs.cmakeFlags;
-    postInstall = ''
-      # put examples in output so doxy_db can test against them
-      cp -r ../examples $examples
-    '';
-    outputs = [ "out" "examples" ];
-  });
-
-  builtExamples = runCommand "all.xml" {
+  builtExamples = runCommand "built_examples" {
     # not really necessary for a package that's in stdenv
-    DOXYGEN_ROOT = "${doxygen_sqlite3}";
-    DOXYGEN_EXAMPLES_DIR = "${doxygen_sqlite3.examples}";
-    nativeBuildInputs = [ libxslt doxygen_sqlite3 ];
+    DOXYGEN_ROOT = "${doxygen}";
+    DOXYGEN_EXAMPLES_DIR = "${doxygen.examples}";
+    nativeBuildInputs = [ libxslt doxygen ];
     examples = ./examples.conf;
   } ''
     doxygen $examples
@@ -70,6 +44,7 @@ in python311Packages.buildPythonPackage rec {
   COLUMNS = 114; # override to tidy output
   preCheck = ''
     # check format
+    # TODO: format has floated, but let's get this all on stable ground before catching up
     # black --check --target-version py36 *.py doxy_db
 
     mkdir -p doxy_db/tests/xml
